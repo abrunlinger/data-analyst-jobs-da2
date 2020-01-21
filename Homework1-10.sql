a70abf516fa02b21de71c50bcc0f9a8576fd4dd3
abrunlinger <59903270+abrunlinger@users.noreply.github.com>
2020-01-21 17:46:42 -0600

    Question10

---
+++ Homework1-10.sql
@@ -0,0 +1,5 @@
+/* 10.	Add the code to order the query in #9 from highest to 
+lowest average star rating. Which company with more than 5000 
+reviews across all locations in the dataset has the highest 
+star rating? What is that rating? Microsoft - 4.199999809
+*/
